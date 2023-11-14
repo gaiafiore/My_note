76bc24e915b5a7cfc2b2791cb698e1b9fb9b1aa0
gaiafiore <gaiafiore.ff@gmail.com>
2023-11-14 12:22:23 +0100

    first commit
    adding project

---
+++ My_note/My_noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_noteApp.swift
+//  My_note
+//
+//  Created by Gaia Francesca Fioretti on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct My_noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
